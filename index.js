fef13d20444c7aef132a657dc42261592cab4f57
Samantha Bora <samantha.babubora@gmail.com>
2025-11-27 19:56:42 +0300

    Completed login tracker function assignment

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 
+// This function creates a login tracker for a specific user.
+// Keeps track of login attempts
+function createLoginTracker(userInfo) {
+  // Counts the number of login attempts.
+  let attemptCount = 0;
 
+  // This inner function runs every time a login attempt happens.
+  return function (passwordAttempt) {
 
+    // If the user already tried 3 times, the account should stay locked.
+    if (attemptCount >= 3) {
+      return 'Account locked due to too many failed login attempts';
+    }
+
+    // Increase attempt count for each password attempt.
+    attemptCount++;
+
+    // If the entered password is correct, login is successful.
+    if (passwordAttempt === userInfo.password) {
+      return 'Login successful';
+    }
+
+    // If the entered password is wrong, return the attempt number.
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+}
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
